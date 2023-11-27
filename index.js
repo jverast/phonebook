cfac1199ab544d6170592bcd8202895f358095bb
jverast <jverast15@gmail.com>
2023-11-27 10:10:31 -0500

    part3 3.3

--- index.js
+++ index.js
@@ -40,6 +40,18 @@ app.get("/info", (req, res) => {
   `)
 })
 
+app.get("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id),
+    person = persons.find((person) => person.id === id)
+
+  if (!person) {
+    res.status(404).json({ error: "not found" })
+    return
+  }
+
+  res.json(person)
+})
+
 app.listen(HTTP_PORT, () => {
   console.log(`App is running on port ${HTTP_PORT}`)
 })
