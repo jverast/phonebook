32be2720acc6533712b8dd19b0b060102295bb83
jverast <jverast15@gmail.com>
2023-11-28 01:13:02 -0500

    part3 3.12

---
+++ mongo.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose")
+
+if (process.argv.length < 3) {
+  console.log(
+    "Please provide the password as an argument: node mongo.js <password>"
+  )
+  process.exit(1)
+}
+
+const password = process.argv[2],
+  db = "phonebookApp",
+  url = `mongodb+srv://M1rcO:${password}@cluster0.1w12vwc.mongodb.net/${db}?retryWrites=true&w=majority`
+
+mongoose.set("strictQuery", false)
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String
+})
+
+const Person = mongoose.model("Person", personSchema)
+
+if (process.argv.length === 3) {
+  console.log("phonebook:")
+  Person.find({}).then((result) => {
+    result.forEach((person) => {
+      console.log(`${person.name} ${person.number}`)
+    })
+    mongoose.connection.close()
+  })
+} else {
+  const name = process.argv[3],
+    number = process.argv[4]
+
+  const person = new Person({
+    name,
+    number
+  })
+
+  person.save().then(() => {
+    console.log(`added ${name} number ${number} to phonebook`)
+    mongoose.connection.close()
+  })
+}
